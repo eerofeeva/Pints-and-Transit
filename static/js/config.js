6feff460ab8e592408755e6800487c8812bfb053
Kelly OlienC:/Users/Kelly/anaconda3/python.exe <kdubois77@hotmail.com>
2021-01-30 11:46:44 -0600

    adding schema

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 //API_key
-MAPBOX_API_KEY = 'pk.eyJ1Ijoic2Nob29sZWEiLCJhIjoiY2trNjVuOXRuMDBxYzJ2bzdsOGVheDhhYSJ9.7RGGtTdaPtiCmofmLFBa6g'
+MAPBOX_API_KEY = 'pk.eyJ1Ijoia2VsbHlvbGllbiIsImEiOiJja2s2NGcxYmwwMGY4MnZvMnVqa21xMmdnIn0.UFBopiYOzLqPPcMWiCTarQ'
 MINNEAPOLIS_CENTER_COORDS=[44.9708543,-93.4015664]
 
 //Websites to scrape
