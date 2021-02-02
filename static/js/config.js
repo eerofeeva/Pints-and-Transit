9d68d080686285580135b68c16c50632eb4c55e2
Elena Erofeeva <eerofeeva8@gmail.com>
2021-02-01 20:42:58 -0600

    WIP: restoring config

---
+++ static/js/config.js
@@ -0,0 +1,20 @@
+//API_key
+const OPEN_BREWERY_API='https://opendata.arcgis.com/datasets/5042131de56d44749f6e43c0b5738b21_0.geojson';
+
+const BREWERY_ONSALE_QUERY_URL = 'https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/On_Sale_Liquor/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+const BREWERY_OFSALE_QUERY_URL = '';
+
+const MAPBOX_API_KEY = 'pk.eyJ1Ijoic2Nob29sZWEiLCJhIjoiY2trNjVuOXRuMDBxYzJ2bzdsOGVheDhhYSJ9.7RGGtTdaPtiCmofmLFBa6g';
+
+const MINNEAPOLIS_CENTER_COORDS=[44.9772845,-93.2676163];
+const STARTING_ZOOM = 14;
+
+
+//Websites to scrape
+EATER='https://twincities.eater.com/'
+
+ABOUT_US_TEXT =''
+
+
+
+//
