3a439d012c0e645cc93d4d18a1a49d348c40cd63
Kylmalcolm <kylie.a.malcolm@gmail.com>
2021-02-03 20:51:37 -0600

    updating scrape

--- Kylie-web-scrape/app.py
+++ Kylie-web-scrape/app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import requests
-import scrape_mars
+import scrape_eater
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/eater_app"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars_content = mongo.db.mars_content.find_one()
-    return render_template("index.html", mars=mars_content)
+    eater_content = mongo.db.eater_content.find_one()
+    return render_template("index.html", eater=eater_content)
 
 @app.route("/scrape")
 def scrape():
-    mars_content = mongo.db.mars_content
-    mars_info = scrape_mars.scrape()
-    mars_content.update({}, mars_info, upsert=True)
+    eater_content = mongo.db.eater_content
+    eater_info = scrape_eater.scrape()
+    eater_content.update({}, eater_info, upsert=True)
     return redirect("/", code=302)
 
 
--- Kylie-web-scrape/scrape_eater.py
+++ Kylie-web-scrape/scrape_eater.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup as bs
-import time
 import requests
 import pymongo
 from splinter import Browser
-import pandas as pd
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -15,92 +13,35 @@ def scrape():
     executable_path = {'executable_path': 'c:/bin/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
     
-    ### NASA Mars News
-    url = 'https://mars.nasa.gov/news/'
+    ### Eater
+    url = 'https://twincities.eater.com/'
 
     browser.visit(url)
     html = browser.html
     soup = bs(html, 'html.parser')
 
-    blurb = soup.select_one('ul.item_list li.slide')
-    title = blurb.find("div", class_='content_title').get_text()
+    titles = soup.find_all("h2", class_="c-entry-box--compact__title")
 
-    news_p = blurb.find("div", class_='rollover_description_inner').get_text()
+    matches = ["Beer", "Brewery", "Breweries", "Wine", "Winery", "Wineries", "Growler", "Growlers", "Cocktail", "Cocktails", "Alcohol", "Bike", "Bikes", "Cycle", "Cyclist", "Cyclists", "Alcohol", "Liquor", "Bar", "Bars"]
+    
+    x = 0
 
-    ### JPL Mars Space Images - Featured Image
-    time.sleep(2)
-    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    browser.visit(url2)
+    article_titles = []
+    links = []
 
-    browser.click_link_by_id('full_image')
-    browser.click_link_by_partial_href('/spaceimages/details')
-
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-    lede = soup.find('figure', class_='lede')
-    link = lede.find('a')
-    href = link['href']
-
-    features_image_url = 'https://www.jpl.nasa.gov' + href
-
-    ### Mars Facts
-    time.sleep(2)
-    url3 = 'https://space-facts.com/mars/'
-    browser.visit(url3)
-
-    tables = pd.read_html(url3)
-
-    type(tables)
-    df = tables[0]
-    df.columns = ['Mars Facts', '']
-
-    df.set_index('Mars Facts', inplace=True)
-
-    html_table = df.to_html()
-    html_table = html_table.replace('\n', '')
-
-    ### Mars Hemispheres
-    time.sleep(2)
-    url4 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url4)
-
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-    dict_list = []
-
-    items = soup.find_all('div', class_='item')
-
-    for item in items:
-        h3 = item.find('h3').get_text()
-        h3 = h3.replace(' Enhanced','')
-        dict = {"title": h3}
-
-        browser.find_by_tag('h3').click()
-        browser.find_by_id('wide-image')
-
-        html = browser.html
-        soup = bs(html, 'html.parser')
-
-        downloads = soup.find('div', class_='downloads')
-        li = downloads.find('li')
-        link1 = li.find('a')
-        href1 = link1['href']
-
-        dict.update({"img_url": href1})
-        dict_list.append(dict)
-
-        browser.visit(url4)
+    for _ in titles:
+        if any(y in titles[x].string for y in matches) == True:
+            article_titles.append(titles[x].string)
+            links.append(titles[x].find('a').get('href'))
+            x = x + 1
+        else:
+            x = x + 1
 
     eater_content = {
-        "news_title": title,
-        "news_p": news_p,
-        "features_image_url": features_image_url,
-        "html_table": html_table,
-        "hemeispheres": dict_list
+        "article_titles": titles,
+        "links": links
     }
 
     browser.quit()
 
-    return mars_content
+    return eater_content
