ae6a232c793d68c0cc2c6c83398576d3e3d82dee
Kelly OlienC:/Users/Kelly/anaconda3/python.exe <kdubois77@hotmail.com>
2021-02-05 20:57:45 -0600

    graph

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,69 +1,70 @@
-// // Fetch the JSON data and console log it
-// const csv = "../../resources/top_50.json";
+// Fetch the JSON data and console log it
+const csv = "../../resources/top_10.csv";
 
-// d3.csv(csv).then(function(data) {
-//     console.log(data);
-//   });
+d3.csv(csv).then(function(data) {
+    console.log(data);
+  });
 
 // d3.json("/plot").then
-//   // create plot
+  // create plot
 
 //   // Define SVG area dimensions
-// var svgWidth = 960;
-// var svgHeight = 660;
+var svgWidth = 960;
+var svgHeight = 660;
 
-// // // Define the chart's margins as an object
-// var chartMargin = {
-//   top: 30,
-//   right: 30,
-//   bottom: 30,
-//   left: 30
-// };
+// // // // Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
 
-// // Define dimensions of the chart area
-// var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-// var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+// // // Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-// // // Select body, append SVG area to it, and set the dimensions
-// var svg = d3
-//   .select("body")
-//   .append("svg")
-//   .attr("height", svgHeight)
-//   .attr("width", svgWidth);
+// // // // Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
 
-// // // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// // // to the margins set in the "chartMargin" object.
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+// // // // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// // // // to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// // Load data from hours-of-tv-watched.csv
-d3.csv("../../resources/top_50.json").then(function(stations) {
 
-//   // Print the tvData
+d3.csv("../../resources/top_10.csv").then(function(stations) {
+
+
   console.log(stations);
 
-//   // Cast the hours value to a number for each piece of tvData
+
   stations.forEach(function(data) {
-    data.Ride_count = +data.Ride_count;
+    // data.Ride_count =+ data.Ride_count;
+    console.log(data.start_station_name)
   });
 
   var barSpacing = 10; // desired space between each bar
   var scaleY = 10; // 10x scale on rect height
 
-// //   // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
+// Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
   var barWidth = (chartWidth - (barSpacing * (stations.length - 1))) / stations.length;
 
-//   // @TODO
-//   // Create code to build the bar chart using the tvData.
+// @TODO
+ // Create code to build the bar chart 
   chartGroup.selectAll(".bar")
     .data(stations)
     .enter()
     .append("rect")
     .classed("bar", true)
-    .attr("width", d => barWidth)
-    .attr("height", d => d.Ride_count * scaleY)
-    .attr("x", (d, i) => i * (barWidth + barSpacing))
-    .attr("y", d => chartHeight - d.Ride_count * scaleY);
+    .attr("width", data => barWidth)
+    .attr("height", data => data.Ride_count * scaleY)
+    .attr("x", (data, i) => i * (barWidth + barSpacing))
+    .attr("y", data => chartHeight - data.Ride_count * scaleY);
 }).catch(function(error) {
   console.log(error);
 });  
