96d9b4f5d3bfbc3cd529781c6de566d4006f6e99
Elena Erofeeva <eerofeeva8@gmail.com>
2021-02-05 21:57:17 -0600

    WIP: changes to fix the resources route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, send_file
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 import requests
 import scrape_eater
 #import pandas as pd
@@ -8,26 +8,32 @@ app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/eater_app"
-mongo = PyMongo(app)
+#mongo = PyMongo(app)
 
-@app.route("/news")
-def index_scrape():
-    eater_content = mongo.db.eater_content.find_one()
-    return render_template("index.html", mars=eater_content)
+# @app.route("/news")
+# def index_scrape():
+#     eater_content = mongo.db.eater_content.find_one()
+#     return render_template("index.html", mars=eater_content)
     #return render_template("index.html", eater=eater_content)
 
-@app.route("/scrape")
-def scrape():
-    eater_content = mongo.db.eater_content
-    eater_info = scrape_eater.scrape()
-    eater_content.update({}, eater_info, upsert=True)
-    return redirect("/", code=302)
+# @app.route("/scrape")
+# def scrape():
+#     eater_content = mongo.db.eater_content
+#     eater_info = scrape_eater.scrape()
+#     eater_content.update({}, eater_info, upsert=True)
+#     return redirect("/", code=302)
 
 @app.route("/")
 def index():
+    print("in index route")
     return send_file("static/index.html")  
-    
-# @app.route("/plot")
+
+@app.route("/resources/top_50.json")  
+def top_50():
+    print("in resources route")
+    return send_file("resources/top_50.json")
+
+#@app.route("/plot")
 #def plot():
     #bike_data = pd.read_csv("resources/all_data_2020.csv", encoding="utf-8")
     #find ride occurances of stations - Pandas HW getting occurances by type 
