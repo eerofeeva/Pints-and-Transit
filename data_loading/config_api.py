df8333085b180c90bb1272143f0e7c5f1bd8f0e0
Elena Erofeeva <eerofeeva8@gmail.com>
2021-02-07 22:26:05 -0600

    WIP: distance calculation for heatmap -DONE

---
+++ data_loading/config_api.py
@@ -0,0 +1,12 @@
+#API_key
+OPEN_BREWERY_API='https://opendata.arcgis.com/datasets/5042131de56d44749f6e43c0b5738b21_0.geojson'
+
+BREWERY_ONSALE_CSV_URL = 'https://opendata.arcgis.com/datasets/5042131de56d44749f6e43c0b5738b21_0.csv'
+BREWERY_OFSALE_CSV_URL = 'https://opendata.arcgis.com/datasets/04284cb2bc4448df83527ae3e33d5ac9_0.csv'
+
+NEIGHBOURHOODS_QUERY_URL = 'https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Minneapolis_Neighborhoods/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+
+MAPBOX_API_KEY = 'pk.eyJ1Ijoia29yeWhhcnJpczciLCJhIjoiY2trNjVkaTd1MDBudDJ2bzcyb3pwd3U2ciJ9.oA0ICkG3v_j_Bc3n_AnTtw'
+
+MINNEAPOLIS_CENTER_COORDS=[44.9772845,-93.2676163]
+STARTING_ZOOM = 14
